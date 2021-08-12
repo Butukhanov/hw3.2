e7cf0343630eea1c5a0aec8197fdd58ad77f1888
Aleksandr Butukhanov <avbutukhanov@gmail.com>
2021-08-12 13:29:56 +0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int topUp = 10000;
+        int total = amount + topUp;
+        boolean giveABonus = topUp >= 1000;
+
+        int bonus;
+        if (giveABonus) {
+            bonus = total / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+        System.out.println(total);
+    }
+
+}
